3c14a64189af85d0547ecd015a91d1d10b40acfe
Allison Bolen <bolena@mail.gvsu.edu>
2018-02-19 23:33:04 -0500

    testing

--- router.c
+++ router.c
@@ -69,9 +69,9 @@ int main(){
         struct sockaddr_ll *addrLL;
         addrLL = (struct sockaddr_ll *)(tmp->ifa_addr);
         printf("\t InterFace MAC: %s\n", ether_ntoa((struct ether_addr*)(addrLL->sll_addr)));
-        interfaces[count].MAC=((struct ether_addr*)(addrLL->sll_addr));
-        interfaces[count].name = ifa_name;
-        interfaces[count].IP = ifa_addr;
+        interfaces[count].MAC[0]=((struct ether_addr*)(addrLL->sll_addr));
+        interfaces[count].name = tmp->ifa_name;
+        interfaces[count].IP[0] = tmp->ifa_addr;
 
         count++;
      	//Bind the socket to the address, so we only get packets
