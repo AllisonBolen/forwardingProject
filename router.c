9320e3aada462f10bfd0a1e74941adafa185f326
Allison Bolen <bolena@mail.gvsu.edu>
2018-03-12 13:21:28 -0400

    edits

--- router.c
+++ router.c
@@ -14,6 +14,7 @@
 #include <arpa/inet.h>
 #include <netinet/ip_icmp.h>
 #include <time.h>
+#include <math.h>
 ///  Allison Bolen, Cade Baker, Andy Hung  ///
 
 /// table structure for storing file data ///
