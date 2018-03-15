0d19719e965caf5fd1304f2a78d0a9cd988070a6
Allison Bolen <bolena@mail.gvsu.edu>
2018-03-15 14:03:18 -0400

    fix

--- router.c
+++ router.c
@@ -143,12 +143,13 @@ int main(){
         // send the thing i need it to send // didnt get to the host we wanted
         // check the message array for the time in each one and then delecte the ones that have been sitting for a while
         // for every packet we delete then send an error for each one send it on the path it came on
-        printf("this is a thing");
+
         int now = (int)time(NULL)*1000; ///current time in miliseconds
         int k;
         struct iphdr ipReq;
         struct iphdr ipResp;
         struct ether_header ethResp, eth;
+        printf("this is a thing");
         printf("\nNow: %d, Stored: %d",now,storedMessage[k].timeMS);
         for(k = 0; k < sizeof(storedMessage); k++){
           if((now - storedMessage[k].timeMS) > 200){
